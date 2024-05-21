2be45d82b2325e8f970a2ef6f954d26243cd0d33
Foksnor <laurensthijssen@live.nl>
2024-05-21 17:05:05 +0200

    Misc fix on variable name in Circuitboard

--- Assets/Cards/CircuitBoard.cs
+++ Assets/Cards/CircuitBoard.cs
@@ -89,17 +89,16 @@ public class CircuitBoard : MonoBehaviour
     public void CalculateAllCards(Character targetCharacter, bool isSetupPhase)
     {
         int previsGridNumber = targetCharacter.PositionInGrid;
-        for (int i = 0; i < activeCards.Count; i++)
+        for (int cardNumber = 0; cardNumber < activeCards.Count; cardNumber++)
         {
             if (isSetupPhase)
             {
                 UpdateCardOrder();
-                AllignCardOnCircuitBoard(i);
+                AllignCardOnCircuitBoard(cardNumber);
             }
             // TODO: voeg toe dat spelers aan het begin van hun beurt een nieuwe hand van kaarten uit hun deck trekt, en dan er een mogen kiezen, daarna moet deze onderstaande functie aangeroepen worden. :)
-            previsGridNumber = activeCards[i].CalculateCard(previsGridNumber, isSetupPhase);
-
-            ToggleInteractableCardStateOnCircuitBoard(i, isSetupPhase);
+            previsGridNumber = activeCards[cardNumber].CalculateCard(previsGridNumber, isSetupPhase);
+            ToggleInteractableCardStateOnCircuitBoard(cardNumber, isSetupPhase);
         }
         ShowCardPrevis(targetCharacter, isSetupPhase);
     }
