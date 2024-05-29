93d0b056ec6ea358716b0fe4043bca7663a998a7
Foksnor <laurensthijssen@live.nl>
2024-05-29 13:44:29 +0200

    Tweaked death vfx

--- Assets/Characters/DeathVFX/DeathVFX.cs
+++ Assets/Characters/DeathVFX/DeathVFX.cs
@@ -21,9 +21,14 @@ public class DeathVFX : MonoBehaviour
         VisualRendererBottom.sprite = sprite;
 
         // Adding force to fake animation to make it seem like the top of the character flies of the bottom
-        Vector3 explosionLocation = new Vector3(transform.position.x + Random.Range(-0.75f, 0.75f), transform.position.y - 1);
-        VisualSplitTop.AddExplosionForce(300, explosionLocation, 3);
+        Vector3 explosionLocation = new Vector3(transform.position.x + 1, transform.position.y + Random.Range(0, -1f));
+        VisualSplitTop.AddExplosionForce(500, explosionLocation, 3);
         VisualSplitTop.AddTorque(explosionLocation);
-        Invoke("(Destroy(gameObject)", 2);
+        Invoke(nameof(Die), 2);
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
     }
 }
