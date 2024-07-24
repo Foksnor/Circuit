4ac4ea4f6e68b3a610aae7131ce12592894d716e
Foksnor <laurensthijssen@live.nl>
2024-07-24 12:14:19 +0200

    Added feature where you can spawn a character from different positions instead of just one

--- Assets/Characters/CharacterSpawnpoint.cs
+++ Assets/Characters/CharacterSpawnpoint.cs
@@ -1,3 +1,4 @@
+using Sirenix.Utilities;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -5,14 +6,27 @@ using UnityEngine;
 
 public class CharacterSpawnpoint : MonoBehaviour
 {
+    // TODO QQQ: Gradualy spawn more enemies when difficulty should increase
     [SerializeField] private Character characterToSpawnHere;
     [Range(1, 100)] public int appareanceChance = 50;
+    [SerializeField] private Transform[] positions;
     private Rect gizmoRect;
 
     private void Awake()
     {
+        // If no positions are defined, default to the transform where this component is attached to
+        if (positions.IsNullOrEmpty())
+        {
+            positions = new Transform[1];
+            positions[0] = transform;
+        }
+
+        // Select a random position
+        int i = UnityEngine.Random.Range(1, positions.Length) - 1;
+
+        // Spawn character if the odds meet at the selected position
         if (UnityEngine.Random.Range(0, 100) <= appareanceChance)
-            SpawnerFunctions.Instance.SpawnSpecificCharacter(characterToSpawnHere, transform.position, characterToSpawnHere.TeamType);
+            SpawnerFunctions.Instance.SpawnSpecificCharacter(characterToSpawnHere, positions[i].position, characterToSpawnHere.TeamType);
     }
 
     private void OnDrawGizmos()
