8c4963db0c41da173273fbeb09850e5d56f29597
Foksnor <laurensthijssen@live.nl>
2024-09-24 17:11:29 +0200

    WIP Saving and loading data through a json file
    + Renamed game name to Cardomancer
    + Progress commit where you can save data through Json files

---
+++ Assets/Saves/JsonDataService.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using UnityEngine;
+using Newtonsoft.Json;
+
+public class JsonDataService : IDataService
+{
+    public bool SaveData<T>(string RelativePath, T Data, bool Encrypted)
+    {
+        string path = Application.persistentDataPath + RelativePath;
+
+        try
+        {
+            if (File.Exists(path))
+            {
+                Debug.Log("Data exists. Deleting old file and writing a new one!");
+                File.Delete(path);
+            }
+
+            using FileStream stream = File.Create(path);
+            // Immediately close the filestream otherwise when saving to it, it will throw an expection saying the file is already in use
+            stream.Close();
+
+            // Convert all the data we have into the Json format
+            File.WriteAllText(path, JsonConvert.SerializeObject(Data));
+            return true;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Unable to save data due to : {e.Message} {e.StackTrace}");
+            return false;
+        }
+    }
+
+    public T LoadData<T>(string RelativePath, bool Encrypted)
+    {
+        string path = Application.persistentDataPath + RelativePath;
+
+        if (!File.Exists(path))
+        {
+            Debug.LogError($"Cannot load file at {path}. File does not exist!");
+            throw new FileNotFoundException($"{path} does not exist!");
+        }
+
+        try
+        {
+            T data = JsonConvert.DeserializeObject<T>(File.ReadAllText(path));
+            return data;
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Failed to load data due to : {e.Message} {e.StackTrace}");
+            throw e;
+        }
+    }
+
+}
