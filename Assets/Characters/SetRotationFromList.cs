9cd4c1ab8b8d0e4cb8d5bc30f5454b1f87877caf
Foksnor <laurensthijssen@live.nl>
2025-04-10 15:13:48 +0200

    Crates now have random rotation off set
    * Fixed an issue where light cookies from the window were not casting shadows too

---
+++ Assets/Characters/SetRotationFromList.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class SetRotationFromList : MonoBehaviour
+{
+    [SerializeField] private List<Vector2> rotations = new();
+    [SerializeField] private Transform targetTransform = null;
+
+    private void Awake()
+    {
+        int rngFromList = Random.Range(0, rotations.Count - 1);
+        float rotValue = Random.Range(rotations[rngFromList].x, rotations[rngFromList].y);
+        targetTransform.eulerAngles = new Vector3(targetTransform.eulerAngles.x, targetTransform.eulerAngles.y, rotValue);
+    }
+}
